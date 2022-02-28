174a6364f0a51163fdfc2e39232847408f854498
Florian Carré <fcarre@ippon.fr>
2022-02-28 07:26:22 +0100

    fix build issue

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { IconlyProvider } from 'react-iconly';
-import { NextUIProvider } from '@nextui-org/react';
 
 import App from './common/primary/app/App';
 
@@ -9,11 +8,9 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NextUIProvider>
-      <IconlyProvider set="bulk" primaryColor="blue" secondaryColor="blue" stroke="bold" size="xlarge">
-        <App />
-      </IconlyProvider>
-    </NextUIProvider>
+    <IconlyProvider set="bulk" primaryColor="blue" secondaryColor="blue" stroke="bold" size="xlarge">
+      <App />
+    </IconlyProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
